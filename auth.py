f924631cb9d5ccdf6dff613ab86d0feef6ae7b87
henrique-giacomini <hggiacomini@gmail.com>
2023-07-30 18:52:46 -0300

    initial commit

--- auth.py
+++ auth.py
@@ -1,15 +1,39 @@
 import httpx
 
-url = 'https://accounts.zoho.com/oauth/v2/token'
 
+def get_Zoho_Oauthtoken(
+    client_id: str = '1000.T6LDKB524O263IW5YWAH741EGWJ0OV',
+    client_secret: str = 'f4271109effcb2e4ca78d227ccf854e881089e8270',
+    code: str = None
+) -> object:
+    """Retorna um objeto _reponse_ contendo o Zoho_Oauthtoken
 
-def get_Zoho_Oauthtoken():
+    Parameters
+    ----------
+    client_id: str
+    client_secret: str
+    code: str
+
+    Returns
+    -------
+    ZohoOauthtoken: object
+    """
+
+    auth_url = 'https://accounts.zoho.com/oauth/v2/token'
     params = {
-        'code': '1000.980a09156c45f38c5352372219c8a037.b77f1e3c5b851d5d97bc1729e66b58ba',
-        'client_id': '1000.T6LDKB524O263IW5YWAH741EGWJ0OV',
-        'client_secret': 'f4271109effcb2e4ca78d227ccf854e881089e8270',
+        'code': code,
+        'client_id': client_id,
+        'client_secret': client_secret,
         'grant_type':  	'authorization_code',
         'redirect_uri': ''
     }
 
-    r = httpx.post(url=url, params=params)
+    r = httpx.post(url=auth_url, params=params)
+
+    if r.status_code == 200:
+        return r
+
+
+client_id = '1000.T6LDKB524O263IW5YWAH741EGWJ0OV',
+client_secret = 'f4271109effcb2e4ca78d227ccf854e881089e8270'
+get_Zoho_Oauthtoken(client_id, client_secret)
